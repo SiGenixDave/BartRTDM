cd73b55fcb18d7e25d52aed62cb50a19ecb9f0c0
Dave <dsmail@sigenix.com>
2016-07-21 09:25:25 -0400

    Fixed minor issue

--- src/RTDMInitialize.c
+++ src/RTDMInitialize.c
@@ -13,6 +13,7 @@
 #include "ptu.h"
 #include "fltinfo.h"
 #else
+#include <stdio.h>
 #include "MyTypes.h"
 #include "usertypes.h"
 #endif
@@ -29,11 +30,11 @@ void InitializeRtdmStream (RtdmXmlStr *rtdmXmlData);
 void RTDMInitialize (TYPE_RTDMSTREAM_IF *interface)
 {
 
-    RtdmXmlStr *rtdmXmlData;
+    RtdmXmlStr *rtdmXmlData = NULL;
 
     /* Read XML file and update all XML and interface parameters. This call must be
-     * performed first because other init functions use parameters read from the
-     * XML config file */
+     * performed first because other initialization functions use parameters read from the
+     * XML configuration file */
     InitializeXML (interface, &rtdmXmlData);
 
     InitializeRtdmStream (rtdmXmlData);
